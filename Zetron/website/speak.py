b0dd89cee20ab53e39838fe0b1951520d544bd1a
JAI KISHAN <109683192+jaikishan-jk@users.noreply.github.com>
2023-07-07 12:21:30 +0530

    Delete Zetron directory

--- Zetron/website/speak.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Get the path to the directory containing the script
-script_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Create a relative path to the data file
-data_path = os.path.join(script_dir, 'data', 'data.txt')
-print(data_path)
